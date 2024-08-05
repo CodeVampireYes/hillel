e3e9482589024e11d3f8a22dde39d389ad017e2d
CodeVampireYes <ar2rfilonenko@gmail.com>
2024-08-05 21:06:06 +0200

    ДЗ (3.1, 3.2, 3.3)

--- hillel3/3.1.py
+++ hillel3/3.1.py
@@ -1,19 +1,21 @@
 first_number = float(input())
 second_number = float(input())
-if second_number == 0:
-    print('На ноль делить нельзя')
-else:
-    sign = input()
+sign = input()
+
+if sign in ('*', '/', '-', '+'):
     if sign == '*':
         print(first_number * second_number)
     elif sign == '/':
-        print(first_number / second_number)
+        if second_number != 0:
+            print(first_number / second_number)
+        else:
+            print('Делить на ноль нельзя')
     elif sign == '-':
         print(first_number - second_number)
     elif sign == '+':
         print(first_number + second_number)
-    else:
-        print("Используйте только знаки: (*, /, -, +)")
+else:
+    print("Используйте только знаки: (*, /, -, +)")
 
 
 
