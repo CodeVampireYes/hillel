f173c4bf49897b5d20b760a06385691cbeefa8b8
CodeVampireYes <ar2rfilonenko@gmail.com>
2024-08-15 15:08:43 +0200

    ДЗ (5.1, 5.2, 5.3)

--- hillel_5/5.1.py
+++ hillel_5/5.1.py
@@ -1,14 +1,14 @@
 import keyword
 
 #input_user = input()
-input_user = "assert_exception"
+input_user = "assert"
 check_digit = not input_user.isidentifier()
 check_up_letters = bool()
 check_space = bool()
 check_registered_word = bool()
 check_double_underscore = bool(input_user.count('_') == len(input_user) and len(input_user) != 1)
 
-for index, el in enumerate(input_user):
+for el in input_user:
     if el.isupper():
         check_up_letters = True
     if el.isspace():
@@ -18,7 +18,11 @@ for el in keyword.kwlist:
     if el == input_user:
         check_registered_word = True
 
-if check_digit or check_up_letters or check_space or check_registered_word or check_double_underscore == True:
+if (check_digit or
+    check_up_letters or
+    check_space or
+    check_registered_word or
+    check_double_underscore):
     print(False)
 else:
     print(True)
