041a78035af9810505b9066a431c14c9626b2c8c
CodeVampireYes <ar2rfilonenko@gmail.com>
2024-08-18 20:49:04 +0200

    ДЗ (5.1, 5.2, 5.3)

--- hillel_5/5.3.py
+++ hillel_5/5.3.py
@@ -1,8 +1,11 @@
 import string
 
-input_user = input('Введите строку:' )
-#input_user = 'Should, I. subscribe? Yes!'
+input_user = 't!e@s#t t%e^s&t'
 
-hashtag_conversion = input_user.title().replace(' ', '').translate(str.maketrans('','', string.punctuation))
+input_clean = input_user.translate(str.maketrans('', '', string.punctuation))
 
-print(f'#{hashtag_conversion[:140]}')
+hashtag_conversion = input_clean.title().replace(' ', '')
+
+hashtag = f'#{hashtag_conversion[:140]}'
+
+print(hashtag)
