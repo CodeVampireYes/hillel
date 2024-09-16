2f3b8837a289b3d51ef98c53ce325942ce355a62
CodeVampireYes <ar2rfilonenko@gmail.com>
2024-09-16 16:27:37 +0200

    ДЗ (13.1, 13,2)

--- hille_13/13.1.py
+++ hille_13/13.1.py
@@ -1,35 +1,35 @@
 class Human:
 
-    def __init__(self, gender, age, first_name, last_name):
+    def __init__(self, gender: str, age: int, first_name: str, last_name: str) -> None:
         self.gender = gender
         self.age = age
         self.first_name = first_name
         self.last_name = last_name
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.gender}, {self.age}, {self.first_name}, {self.last_name}'
 
 
 class Student(Human):
 
-    def __init__(self, gender, age, first_name, last_name, record_book):
+    def __init__(self, gender: str, age: int, first_name: str, last_name: str, record_book: str) -> None:
         super().__init__(gender, age, first_name, last_name)
         self.record_book = record_book
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{super().__str__()}, {self.record_book}'
 
 
 class Group:
 
-    def __init__(self, number):
+    def __init__(self, number: str) -> None:
         self.number = number
         self.group = set()
 
-    def add_student(self, student):
+    def add_student(self, student) -> None:
         self.group.add(student)
 
-    def delete_student(self, last_name):
+    def delete_student(self, last_name: str) -> None:
         student_to_remove = None
         for student in self.group:
             if student.last_name == last_name:
@@ -38,13 +38,13 @@ class Group:
             if student_to_remove:
                 self.group.remove(student_to_remove)
 
-    def find_student(self, last_name):
+    def find_student(self, last_name: str) -> None:
         for student in self.group:
             if student.last_name == last_name:
                 return student
         return None
 
-    def __str__(self):
+    def __str__(self) -> str:
         all_students = '\n'.join([str(student) for student in self.group])
         return f'Group Number: {self.number}\nStudents:\n{all_students}'
 
